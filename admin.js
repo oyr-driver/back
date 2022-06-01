34b104ed02eb296517a357184c3a347ec56644f5
soyoonjeong <76814748+soyoonjeong@users.noreply.github.com>
2022-06-01 16:53:46 +0900

    add send message button

--- admin.js
+++ admin.js
@@ -4,64 +4,61 @@ const express = require("express");
 const { Database, Resource } = require("@adminjs/prisma");
 const {PrismaClient} = require("@prisma/client");
 const PORT = process.env.port || 3000;
-export const prisma = new PrismaClient();
-
+const prisma = new PrismaClient();
+//export
 AdminJS.registerAdapter({ Database, Resource });
 
 const run = async () => {
     const app = express();
     // `_dmmf` contains necessary Model metadata. `PrismaClient` type doesn't have it included
-    const dmmf = prisma._dmmf;
-    console.log(prisma._dmmf.modelMap);
-    const resources = Object.keys(dmmf.modelMap)
-        .map((val, idx) => dmmf.modelMap[val])
-        .map((model) => ({
-        resource: { model, client: prisma },
-        options: {},
-         }));
-    resources[1].options = { //call에 send Message 액션 추가
-      actions:{
-        newAction:{
-          name:'Send Message',
-          actionType :'record',
-          isVisible:true,
-          handler:async(req,res,data)=>{
-            if(!data.record){
-              throw new NotFoundError([
-                `Record of given id("${req.params.recordId}")could not be found`,
-              ].join('\n'), 'Action#handler')
-            }
-            return{
-              record:data.record.toJSON(data.currentAdmin)
-            }
-
-          },
-        }
-      }
-    };
-    console.log(resources[1].options.actions.newAction);
+    const dmmf = prisma._dmmf.modelMap;
     const adminJS = new AdminJS({
         databases :[prisma], //DB 연결
         rootPath : '/admin',
-        resources : {resources}
-    });
-    const ADMIN = { //로그인 정보
-        email : 'gooddrive@gmail.com',
-        password : 'gooddrive'
-    }
-
-    const router = AdminJSExpress.buildAuthenticatedRouter(adminJS, {
-        authenticate: async (email, password) => {
-          if (ADMIN.password === password && ADMIN.email === email) {
-            return ADMIN
-          }
-            return null
+        resources : [
+          {resource:{model:dmmf.auth, client:prisma},
+            options:{}
           },
-        cookieName: 'adminJS',
-        cookiePassword: 'testtest'
-      }); 
 
-    //const router = AdminJSExpress.buildRouter(adminJS);
+          {resource:{model:dmmf.call, client:prisma},
+            options:{ //call에 send Message 액션 추가
+              actions:{
+                list, edit, new:{
+                  showInDrawer:true,
+                },
+                newAction:{
+                  name:'send Message',
+                  actionType: 'record',
+                  isVisible: true,
+                  icon:'Edit',
+                  handler: () => {},
+                  component: false,
+                }
+              }
+            }
+          },
+          {resource:{model:dmmf.user, client:prisma},
+            options:{}
+          }
+        ]
+    });
+
+    // const ADMIN = { //로그인 정보
+    //     email : 'gooddrive@gmail.com',
+    //     password : 'gooddrive'
+    // }
+    // const router = AdminJSExpress.buildAuthenticatedRouter(adminJS, {
+    //     authenticate: async (email, password) => {
+    //       if (ADMIN.password === password && ADMIN.email === email) {
+    //         return ADMIN
+    //       }
+    //         return null
+    //       },
+    //     cookieName: 'adminJS',
+    //     cookiePassword: 'testtest'
+    //   }); 
+
+    const router = AdminJSExpress.buildRouter(adminJS);
     app.use(adminJS.options.rootPath, router);
     app.listen(PORT, () => {
         console.log(`Example app listening at http://localhost:${PORT}`);
